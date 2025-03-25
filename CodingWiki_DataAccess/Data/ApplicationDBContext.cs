9b2be8f550b0283f5776bd00b612c819d492c713
IgorHirsch <g1rsh.egor@gmail.com>
2025-03-25 17:13:01 +0100

    2. Migrations

---
+++ CodingWiki_DataAccess/Data/ApplicationDBContext.cs
@@ -0,0 +1,37 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        // Keine Construktor
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=SKYNET;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "Spider without Duty", ISBN = "123B12", Price = 10.99m },
+                new Book { BookId = 2, Title = "Fortune of time", ISBN = "12123B12", Price = 11.99m }
+            );
+
+            var bookList = new Book[]
+            {
+                new Book { BookId = 3, Title = "Fake Sunday", ISBN = "77652", Price = 20.99m },
+                new Book { BookId = 4, Title = "Cookie Jar", ISBN = "CC12B12", Price = 25.99m },
+                new Book { BookId = 5, Title = "Cloudy Forest", ISBN = "90392B33", Price = 40.99m }
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
+        }
+    }
+}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CodingWiki_Model.Models
+{
+   public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+    }
+}
