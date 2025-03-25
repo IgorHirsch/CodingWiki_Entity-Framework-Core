ac7555f845a682a894a9820a973991b1b0bb68d8
IgorHirsch <g1rsh.egor@gmail.com>
2025-03-25 18:39:42 +0100

    3. Data Annotations

--- CodingWiki_DataAccess/Data/ApplicationDBContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDBContext.cs
@@ -6,7 +6,13 @@ namespace CodingWiki_DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
-        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+
+
 
         // Keine Construktor
 
--- CodingWiki_Model/Models/Book.cs
+++ CodingWiki_Model/Models/Book.cs
@@ -1,14 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace CodingWiki_Model.Models
 {
-   public class Book
+    public class Book
     {
-        [Key]
+        // [Key]
         public int BookId { get; set; }
         public string Title { get; set; }
+
+        [MaxLength(20)]
+        [Required]
         public string ISBN { get; set; }
+
         public decimal Price { get; set; }
+
+        [NotMapped]
+        public string PriceRange { get; set; }
     }
 }
